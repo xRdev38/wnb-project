e705fcd098c60759969d44ec1f1296283b573816
xRdev38 <developer.rudy@gmail.com>
2022-07-30 11:14:38 +0400

    feat(shared:card): test ng projection

--- src/app/shared/components/card/card.component.spec.ts
+++ src/app/shared/components/card/card.component.spec.ts
@@ -1,22 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-mock-test',
+  template: `
+    <app-card>
+      <div card-body-left>I'm the Left</div>
+      <span card-body-right>I'm the right</span>
+    </app-card>
+  `,
+})
+class MockTestCardComponent {}
 
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+  let component: MockTestCardComponent;
+  let fixture: ComponentFixture<MockTestCardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
-    })
-    .compileComponents();
+      declarations: [CardComponent, MockTestCardComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
+    fixture = TestBed.createComponent(MockTestCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should correctly project the right content', () => {
+    expect(
+      fixture.debugElement.query(By.css('app-card')).query(By.css('span'))
+        .nativeElement.innerHTML
+    ).toContain('I\'m the right');
+  });
+
+  it('should correctly project the left content', () => {
+    expect(
+      fixture.debugElement.query(By.css('app-card')).query(By.css('div'))
+        .nativeElement.innerHTML
+    ).toContain('I\'m the Left');
   });
 
   it('should create', () => {
