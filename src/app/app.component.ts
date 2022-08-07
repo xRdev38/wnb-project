b83ef2ba6571bdd9f752703288ecce86465318a3
xRdev38 <developer.rudy@gmail.com>
2022-08-07 14:28:01 +0400

    refactor: add global loader for the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OverlayService } from '@core/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wnb-interview-mini-project';
+  isLoading$: Observable<boolean>;
+
+  constructor(private readonly overlayService: OverlayService) {}
+
+  ngOnInit() {
+    this.isLoading$ = this.overlayService.isLoading;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from '@core/core.module';
+import { OverlayService } from '@core/services';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule],
-  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }, OverlayService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
