844a8205e0b9893872bdc6b8caf01bcd21cbecae
xRdev38 <developer.rudy@gmail.com>
2022-08-07 14:23:47 +0400

    refactor(shared): improve spinner

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BaseComponent } from '../base.component';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpinnerComponent extends BaseComponent {
+  @Input() set size(data: number) {
+    this.sizeStyle = data;
+  }
+
+  get size(): number {
+    return this.sizeStyle;
+  }
+
+  private sizeStyle = 2;
+
   constructor() {
     super();
   }
